0beb72c79df8ded6b4360a8cff23edbcd8e1def4
Sydney Havengar <sydney.havengar01@gmail.com>
2022-02-23 16:47:53 -0600

    adds navlinks to menu and routing in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import LoginPage from "./components/LoginPage";
+import ProfilePage from "./components/ProfilePage";
+import CreateNewPage from "./components/CreateNewPage";
+import ChallengePage from "./components/ChallengePage";
+import HistoryPage from "./components/HistoryPage";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <Router>
+            <Menu />
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/create" element={<CreateNewPage />} />
+                <Route path="/challenge" element={<ChallengePage />} />
+                <Route path="/history" element={<HistoryPage />} />
+                <Route path="/museum" element={<MuseumPage />} />
+                <Route path="/friends" element={<FriendsPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return <div>Menu</div>;
+    return (
+        <nav>
+            <NavLink to="login">Login</NavLink>
+            <NavLink to="profile">Profile</NavLink>
+            <NavLink to="create">Create Challenge</NavLink>
+            <NavLink to="challenge">Challenge</NavLink>
+            <NavLink to="history">History</NavLink>
+            <NavLink to="museum">Museum</NavLink>
+            <NavLink to="friends">Friends</NavLink>
+        </nav>
+    );
 };
 
 export default Menu;
