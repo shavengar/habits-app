e18955ddbbf4d2ba4fa8142c60ee2a93b5a15321
Sydney Havengar <sydney.havengar01@gmail.com>
2022-02-25 16:06:22 -0600

    troubleshooting

--- src/components/CreateNewPage.jsx
+++ src/components/CreateNewPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const CreateNewPage = () => {
-    return <div>Create New Page</div>;
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null);
+    const onChange = (dates) => {
+        const [start, end] = dates;
+        setStartDate(start);
+        setEndDate(end);
+    };
+    return (
+        <div>
+            <h3>Create a new challenge:</h3>
+            <label htmlFor="habitLabel">Habit:</label>
+            <input id="habitLabel" type="text" />
+            <br />
+            <label htmlFor="dateRange">Length of habit:</label>
+            <DatePicker
+                selected={startDate}
+                onChange={onChange}
+                startDate={startDate}
+                endDate={endDate}
+                selectsRange
+                inline
+            />
+        </div>
+    );
 };
 
 export default CreateNewPage;
