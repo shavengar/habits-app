5a314dbd6c964fc73f6e1676a3ba1ad4fd9f163d
Sydney Havengar <sydney.havengar01@gmail.com>
2022-02-25 09:08:31 -0600

    adds challenge actions and reducer

--- src/redux/reducers/challenge.reducer.js
+++ src/redux/reducers/challenge.reducer.js
@@ -1,27 +1,57 @@
-import { ADD_CHALLENGE, REMOVE_CHALLENGE, EDIT_TITLE, ADD_START_DATE, ADD_END_DATE, ADD_COMPLETED_DAY, REMOVE_COMPLETED_DAY, ADD_IMAGES, CREATE_ART, REMOVE_ART  } from "../actions"
+import {
+    ADD_CHALLENGE, 
+    REMOVE_CHALLENGE, 
+    ADD_COMPLETED_DAY, 
+    REMOVE_COMPLETED_DAY
+} from "../actions"
 
 const initialState = {
     challenge = {
         startDate: null, 
         endDate: null, 
-        id: null, 
         title: "",
+        id: null,
         completedDays: []
     },
     completedDay = {
         date: null,
         image:{
-            art: {
-                url: null,
-                title: "",
-                artist: "",
-                id: ""
-            },
-            placeholder: {
-                url: null
-            },
+            url: null
         },
-        completed: false,
+        challengeID: null,
     },
     challenges = [],
+    completedDays = [],
+    completedChallenges = [],
 }
+
+const challengeReducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_CHALLENGE:
+            return {...state, challenges: [...state.challenges, action.challenge]}
+        case REMOVE_CHALLENGE:
+            return {
+                ...state, 
+                challenges: state.challenges.filter((challenge) => challenge.id !== action.id),
+            };
+        case COMPLETE_CHALLENGE:
+            return {
+                ...state,
+                completedChallenges: [...state.completedChallenges, state.challenges.filter((challenge) => challenge.id === action.id)],
+            };
+        case ADD_COMPLETED_DAY:
+            return {
+                ...state,
+                completedDays: [...state, action.completedDay]
+            }
+        case REMOVE_COMPLETED_DAY:
+            return {
+                ...state,
+                completedDays: state.completedDays.filter((completedDay) => completedDay.challengeID !== action.challengeID)
+            }
+        default:
+            return state;
+    }
+}
+
+export default challengeReducer;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
+import challengeReducer from "./challenge.reducer";
 
 const rootReducer = combineReducers({
     user: userReducer,
+    challenge: challengeReducer,
 });
 
 export default rootReducer;
