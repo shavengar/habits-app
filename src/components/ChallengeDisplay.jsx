375ea71ab5416bd05a358bb9bc086d09f250d3a5
Sydney Havengar <sydney.havengar01@gmail.com>
2022-02-27 18:27:58 -0600

    troubleshooting

--- src/components/ChallengeDisplay.jsx
+++ src/components/ChallengeDisplay.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-const ChallengeDisplay = () => {
-    return <div>Challenge Display</div>;
+const ChallengeDisplay = ({
+    project,
+    projects,
+    markCompleted,
+    removeProject,
+    completeProject,
+}) => {
+    return (
+        <div className="projects">
+            <div
+                className={
+                    project.completed ? "completeProject" : "incompleteProject"
+                }
+            >
+                <h3>{project.title}</h3>
+                <p>{project.dueDate}</p>
+                <label>Completed:</label>
+                <input
+                    type="checkbox"
+                    onChange={() => {
+                        markCompleted(project.id);
+                        console.log(projects);
+                    }}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default ChallengeDisplay;
--- src/components/ChallengePage.jsx
+++ src/components/ChallengePage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
+import { completeProject, removeProject } from "../redux/actions";
+import ChallengeDisplay from "./ChallengeDisplay";
 
-const ChallengePage = () => {
-    return <div>Challenge Page</div>;
+const ChallengePage = ({ projects, removeProject, completeProject }) => {
+    const markCompleted = (id) => {
+        completeProject((curr) => {
+            curr.map((project) => {
+                if (id === project.id) {
+                    return { ...project, completed: !project.completed };
+                }
+                return project;
+            });
+        });
+    };
+    return (
+        <div>
+            <h2>Challenges:</h2>
+            {projects.map((val) => (
+                <ChallengeDisplay
+                    key={val.id}
+                    project={val}
+                    removeProject={removeProject}
+                    completeProject={completeProject}
+                    markCompleted={markCompleted}
+                    projects={projects}
+                />
+            ))}
+        </div>
+    );
 };
 
-export default ChallengePage;
+const mapStateToProps = (state) => {
+    return { projects: state.challenge.projects };
+};
+const mapDispatchToProps = {
+    removeProject,
+    completeProject,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChallengePage);
