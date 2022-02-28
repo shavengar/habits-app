f32f95291b1c637333863145e127cf9d69df0c08
Sydney Havengar <sydney.havengar01@gmail.com>
2022-02-28 08:41:36 -0600

    adds api call to grab random artwork

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,5 +1,32 @@
 import React, { useState, useEffect } from "react";
 
-const useFetch = () => {};
+const baseURL =
+    "https://www.rijksmuseum.nl/api/en/collection?key=L93w4I9H&imgonly=true&ps=100&toppieces=true";
+
+const useFetch = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function loadData() {
+            setData(null);
+            setError(null);
+            let randomIndex = math.Floor(math.Random() * (100 - 1) + 1);
+            try {
+                const response = await fetch(baseURL);
+                const json = await response.json();
+                const art = json.artObjects.at(randomIndex).map((val) => ({
+                    id: val.id,
+                    title: val.longTitle,
+                    img: val.webImage.url,
+                }));
+                setData(art);
+            } catch (err) {
+                setError(err);
+            }
+        }
+    }, [completedProjects]);
+    return { data, error };
+};
 
 export default useFetch;
