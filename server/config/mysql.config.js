79dcc3cf3dbc4d7be0e03ab104724ed02876d45e
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-06 13:02:05 -0600

    adds handling for connections to database

---
+++ server/config/mysql.config.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+const util = require("util");
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+            console.error("Database connection was closed.");
+        }
+        if (err.code === "ER_CON_COUNT_ERROR") {
+            console.error("Database has too many connections.");
+        }
+        if (err.code === "ECONNREFUSED") {
+            console.error("Database connection was refused.");
+        }
+    }
+    if (connection) connection.release();
+});
+
+const query = util.promisify(pool.query).bind(pool);
+
+module.exports = query;
