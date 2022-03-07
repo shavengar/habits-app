80c490d1bf493dfa0db3fb45f485378cd9b10c65
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-06 21:20:31 -0600

    adds routing for habits

---
+++ server/routes/habits.routes.js
@@ -0,0 +1,28 @@
+const express = ("express");
+const router = express.Router();
+const { addHabit, removeHabit, markComplete, getHabitsByUserId };
+
+router.put("/add", (req, res) => {
+    const {user_id, title, completed, due_date} = req.body;
+    if (!user_id || !title || !due_date){
+        return res.send({
+            data: null,
+            success: false,
+            error: "Invalid data provided.",
+        })
+    }
+    const habit = {user_id, title, completed, due_date}
+    return addHabit(res, habit);
+});
+
+router.delete("/remove/:id", (req, res) => {
+    removeHabit(res, req.params.id);
+});
+
+router.patch("/complete/:id", (req, res) => {
+    markComplete(res, req.params.id);
+});
+
+router.get("/:user_id", (req, res) => {
+    getHabitsByUserId(res, req.params.user_id);
+})
