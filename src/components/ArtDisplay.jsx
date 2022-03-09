12773cec44d15cf5d0731f50c76d54b1dc157c10
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-09 15:47:48 -0600

    adds art display functionality

--- src/components/ArtDisplay.jsx
+++ src/components/ArtDisplay.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const ArtDisplay = () => {
-    return <div>Art Display</div>;
+const ArtDisplay = ({ art }) => {
+    console.log(art);
+    return (
+        <div>
+            <div>
+                <img src={art.url} alt="artwork" />
+                <h3>{art.title}</h3>
+            </div>
+        </div>
+    );
 };
 
 export default ArtDisplay;
