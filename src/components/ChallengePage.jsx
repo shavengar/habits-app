61e29f17b9693caa9844b4a5c2837c14cb195754
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-09 15:48:45 -0600

    adds api call to mark projects as complete

--- src/components/ChallengePage.jsx
+++ src/components/ChallengePage.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { connect } from "react-redux";
 import { completeProject } from "../redux/actions";
+import { addArt } from "../redux/actions";
 import ChallengeDisplay from "./ChallengeDisplay";
+import useAPI from "../hooks/useAPI";
 
-const ChallengePage = ({ projects, completeProject }) => {
-    const markCompleted = (id) => {
-        completeProject(id);
-    };
+const ChallengePage = ({ projects, completeProject, addArt }) => {
+    const { markComplete } = useAPI();
+    const markCompleted = useCallback(async (id) => {
+        const res = await markComplete(id);
+        if (!res.data.success) {
+            console.log(res.data.error);
+        } else {
+            completeProject(res.data.data);
+            addArt(res.data.data);
+        }
+    }, []);
     return (
         <div>
             <h2>Challenges:</h2>
@@ -22,10 +31,13 @@ const ChallengePage = ({ projects, completeProject }) => {
 };
 
 const mapStateToProps = (state) => {
-    return { projects: state.challenge.projects };
+    return {
+        projects: state.challenge.projects,
+    };
 };
 const mapDispatchToProps = {
     completeProject,
+    addArt,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChallengePage);
