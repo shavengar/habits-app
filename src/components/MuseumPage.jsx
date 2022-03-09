e116c983afc8367f966a5ec5b53b3a998fa6aec3
Sydney Havengar <sydney.havengar01@gmail.com>
2022-03-09 15:49:14 -0600

    adds mapping through art collection to display pg

--- src/components/MuseumPage.jsx
+++ src/components/MuseumPage.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { connect } from "react-redux";
+import { removeArt } from "../redux/actions";
+import useAPI from "../hooks/useAPI";
+import ArtDisplay from "./ArtDisplay";
 
-const MuseumPage = () => {
-    return <div>Museum Page</div>;
+const MuseumPage = ({ user, artCollection }) => {
+    const { deleteArt } = useAPI();
+    const remove = useCallback(
+        async (art_id) => {
+            const res = await deleteArt(art_id, user.id);
+            if (!res.data.success) {
+                console.log(res.data.error);
+            } else {
+                removeArt(art_id);
+            }
+        },
+        [removeArt, deleteArt]
+    );
+
+    return (
+        <div>
+            <h2>Art Collection:</h2>
+            {artCollection.map((val) => {
+                <ArtDisplay key={val.id} art={val} />;
+            })}
+        </div>
+    );
 };
 
-export default MuseumPage;
+const mapStateToProps = (state) => {
+    return { artCollection: state.art.artCollection, user: state.user };
+};
+
+const mapDispatchToProps = {
+    removeArt,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MuseumPage);
