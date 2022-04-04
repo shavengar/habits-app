98f8327a21b74e77d57307997c4116ba0cc95425
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-04 09:09:44 -0500

    adds authentication middleware to routes

--- server/routes/art.routes.js
+++ server/routes/art.routes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const { removeArt, getArtByHabitId } = require("../models/art.models");
+const authenticate = require("../middleware/authentication.middleware");
 const router = express.Router();
 
-router.delete("/remove/:id", (req, res) => {
-    removeArt(res, req.params.id);
+router.delete("/remove/:id", authenticate, (req, res) => {
+  removeArt(res, req.params.id);
 });
 
+<<<<<<< Updated upstream
 router.post("/", (req, res) => {
     getArtByHabitId(res, req.body);
+=======
+router.get("/:habit_id", authenticate, (req, res) => {
+  getArtByHabitId(res, req.params.habit_id);
+>>>>>>> Stashed changes
 });
 
 module.exports = router;
