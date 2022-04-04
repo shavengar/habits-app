537ebe7b54076e27496241352c7c79d892249b67
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-04 09:10:50 -0500

    adds logout route and verify

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const validate = require("../middleware/validate.middleware");
+const authenticate = require("../middleware/authentication.middleware");
 const { login, signup } = require("../models/auth.models");
 const router = express.Router();
 
+router.get("/verify", authenticate, (req, res) => {
+  return res.send({
+    data: { username: req.user, username },
+    success: true,
+    error: null,
+  });
+});
+
 router.post("/login", validate, (req, res) => {
-    login(res, req.body.username, req.body.password);
+  login(res, req.body.username, req.body.password);
 });
 
 router.put("/signup", validate, (req, res) => {
-    signup(res, req.body.username, req.body.password);
+  signup(res, req.body.username, req.body.password);
+});
+
+router.get("/logout", (req, res) => {
+  res.clearCookie("acess_token");
+  return res.send({
+    data: null,
+    success: true,
+    error: null,
+  });
 });
 
 module.exports = router;
