dfba49c5e854687067454bca0eb96abeac38c279
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 23:09:37 -0500

    adds styling to login page with mui components

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,46 +1,116 @@
-import React, { useCallback } from "react";
-import { setUser } from "../redux/actions";
-import { useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import useAPI from "../hooks/useAPI";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../redux/actions";
+import useAPI from "../hooks/useAPI";
+import vangogh from "../shared/vangogh.png";
+import {
+  Typography,
+  Alert,
+  AlertTitle,
+  Button,
+  TextField,
+} from "@mui/material";
 
 const LoginPage = ({ setUser }) => {
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+  const [requirements, setRequirements] = useState(false);
+  const [inUse, setInUse] = useState(false);
   const { login } = useAPI();
-  const usernameInput = useRef(null);
-  const passwordInput = useRef(null);
   let navigate = useNavigate();
 
   const handleLogin = useCallback(async () => {
-    const username = usernameInput.current.value;
-    const password = passwordInput.current.value;
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
     if (
       username.length < 4 ||
       username.length > 15 ||
       password.length < 4 ||
       password.length > 15
     ) {
-      return;
+      return setRequirements(true);
     }
     const res = await login(username, password);
     if (!res.data.success) {
       console.log(res.data.error);
     } else {
+      setRequirements(false);
+      setInUse(false);
       setUser(res.data.data);
       navigate("/challenge");
     }
   }, []);
 
   return (
-    <div>
-      <h3>Login:</h3>
-      <label htmlFor="username">Username:</label>
-      <input type="text" id="username" ref={usernameInput} />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input type="password" id="password" ref={passwordInput} />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <section className="entryPage displayFlex justifyCenter">
+      <div className="entryContainer displayFlex justifyCenter centerAlign">
+        <div className="entryFunctionality accentColor borderRadius displayFlex justifyCenter centerAlign">
+          <div className="accentColor displayFlex justifyCenter column">
+            <h2 className="textColor">Login:</h2>
+            <TextField
+              id="enterUsername"
+              label="Username"
+              variant="outlined"
+              color="secondary"
+              inputRef={usernameRef}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              id="enterPassword"
+              label="Password"
+              variant="outlined"
+              color="secondary"
+              type="password"
+              inputRef={passwordRef}
+              sx={{ mb: 2 }}
+            />
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                handleLogin();
+              }}
+              sx={{ mb: 2, display: "flex", size: "large" }}
+            >
+              LOGIN
+            </Button>
+            {requirements && (
+              <Alert severity="info" sx={{ mb: 2 }}>
+                <AlertTitle>Info</AlertTitle>
+                <Typography sx={{ fontWeight: 700, fontSize: 14 }}>
+                  Username Requirements:
+                </Typography>
+                <Typography sx={{ fontSize: 14 }}>
+                  - Must have at least 5 characters
+                </Typography>
+                <Typography sx={{ fontSize: 14 }}>
+                  - Must have fewer than 20 characters
+                </Typography>
+                <Typography sx={{ fontWeight: 700, fontSize: 14 }}>
+                  Password Requirements:
+                </Typography>
+                <Typography sx={{ fontSize: 14 }}>
+                  - Must have at least 8 characters
+                </Typography>
+                <Typography sx={{ fontSize: 14 }}>
+                  - Must have fewer than 20 characters
+                </Typography>
+              </Alert>
+            )}
+            {inUse && (
+              <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                <Typography sx={{ fontWeight: 700, fontSize: 14 }}>
+                  Username already in use.
+                </Typography>
+              </Alert>
+            )}
+          </div>
+        </div>
+        <img className="entryImg borderRadius" src={vangogh} />
+      </div>
+    </section>
   );
 };
 
