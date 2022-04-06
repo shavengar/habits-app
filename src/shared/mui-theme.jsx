0a605769a95ec9f8d38980b53239ca50dd394fee
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 22:36:32 -0500

    adds mui theming

---
+++ src/shared/mui-theme.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#FFE299",
+    },
+    secondary: {
+      main: "#575D90",
+    },
+    background: {
+      default: "#575D90",
+    },
+  },
+});
