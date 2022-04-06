7689402de5c676d45622205ac4b8c92b6b1de55f
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 23:11:06 -0500

    fixes bug (changes redirect to challenge page)

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,18 +3,18 @@ import { Navigate } from "react-router-dom";
 import { connect } from "react-redux";
 
 const ProtectedRoute = ({ isPrivate, children, user }) => {
-    const redirectTo = isPrivate ? "/login" : "/profile";
-    if ((user && isPrivate) || (!user && !isPrivate)) {
-        return <>{children}</>;
-    } else {
-        return <Navigate to={redirectTo} />;
-    }
+  const redirectTo = isPrivate ? "/login" : "/challenge";
+  if ((user && isPrivate) || (!user && !isPrivate)) {
+    return <>{children}</>;
+  } else {
+    return <Navigate to={redirectTo} />;
+  }
 };
 
 const mapStateToProps = (state) => {
-    return {
-        user: state.user,
-    };
+  return {
+    user: state.user,
+  };
 };
 
 const mapDispatchToProps = {};
