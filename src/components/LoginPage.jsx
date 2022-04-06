5dd4faecee455c5fd2169ff3c397a4e061caac18
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 21:39:37 -0500

    updates navigation when logged in

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -6,46 +6,46 @@ import useAPI from "../hooks/useAPI";
 import { connect } from "react-redux";
 
 const LoginPage = ({ setUser }) => {
-    const { login } = useAPI();
-    const usernameInput = useRef(null);
-    const passwordInput = useRef(null);
-    let navigate = useNavigate();
+  const { login } = useAPI();
+  const usernameInput = useRef(null);
+  const passwordInput = useRef(null);
+  let navigate = useNavigate();
 
-    const handleLogin = useCallback(async () => {
-        const username = usernameInput.current.value;
-        const password = passwordInput.current.value;
-        if (
-            username.length < 4 ||
-            username.length > 15 ||
-            password.length < 4 ||
-            password.length > 15
-        ) {
-            return;
-        }
-        const res = await login(username, password);
-        if (!res.data.success) {
-            console.log(res.data.error);
-        } else {
-            setUser(res.data.data);
-            navigate("/profile");
-        }
-    }, []);
+  const handleLogin = useCallback(async () => {
+    const username = usernameInput.current.value;
+    const password = passwordInput.current.value;
+    if (
+      username.length < 4 ||
+      username.length > 15 ||
+      password.length < 4 ||
+      password.length > 15
+    ) {
+      return;
+    }
+    const res = await login(username, password);
+    if (!res.data.success) {
+      console.log(res.data.error);
+    } else {
+      setUser(res.data.data);
+      navigate("/challenge");
+    }
+  }, []);
 
-    return (
-        <div>
-            <h3>Login:</h3>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" ref={usernameInput} />
-            <br />
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" ref={passwordInput} />
-            <button onClick={handleLogin}>Login</button>
-        </div>
-    );
+  return (
+    <div>
+      <h3>Login:</h3>
+      <label htmlFor="username">Username:</label>
+      <input type="text" id="username" ref={usernameInput} />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" ref={passwordInput} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
 };
 
 const mapStateToProps = () => {
-    return {};
+  return {};
 };
 
 const mapDispatchToProps = { setUser };
