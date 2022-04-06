f8aac0912b27f7ec602a06ea3b7d9e8ce13b46a2
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 19:36:40 -0500

    adds stashed changes

--- server/routes/art.routes.js
+++ server/routes/art.routes.js
@@ -7,13 +7,8 @@ router.delete("/remove/:id", authenticate, (req, res) => {
   removeArt(res, req.params.id);
 });
 
-<<<<<<< Updated upstream
-router.post("/", (req, res) => {
-    getArtByHabitId(res, req.body);
-=======
 router.get("/:habit_id", authenticate, (req, res) => {
   getArtByHabitId(res, req.params.habit_id);
->>>>>>> Stashed changes
 });
 
 module.exports = router;
