152323fe2443528eb49554b6051a6ec72cb4d693
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 21:58:18 -0500

    adds add completed and remove completed

--- src/redux/actions/challenge.actions.js
+++ src/redux/actions/challenge.actions.js
@@ -1,20 +1,24 @@
 export const ADD_PROJECT = "Add Project";
 export const REMOVE_PROJECT = "Remove Project";
-export const COMPLETE_PROJECT = "Complete Project";
+export const ADD_COMPLETED = "Add Completed";
+export const REMOVE_COMPLETED = "Remove Completed";
 export const SET_PROJECTS = "Set Projects";
 
 export const addProject = (project) => {
-    return {
-        type: ADD_PROJECT,
-        project: project,
-    };
+  return {
+    type: ADD_PROJECT,
+    project: project,
+  };
 };
 export const removeProject = (id) => {
-    return { type: REMOVE_PROJECT, id };
+  return { type: REMOVE_PROJECT, id };
 };
-export const completeProject = (id) => {
-    return { type: COMPLETE_PROJECT, id };
+export const addCompleted = (project) => {
+  return { type: ADD_COMPLETED, project };
+};
+export const removeCompleted = (id) => {
+  return { type: REMOVE_COMPLETED, id };
 };
 export const setProjects = (projects) => {
-    return { type: SET_PROJECTS, projects };
+  return { type: SET_PROJECTS, projects };
 };
