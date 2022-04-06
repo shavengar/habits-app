ccf17c3cd7c379849d66523625abfb496a2cc001
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-05 21:57:29 -0500

    adds a better display of challenges

--- src/components/ChallengePage.jsx
+++ src/components/ChallengePage.jsx
@@ -1,43 +1,55 @@
 import React, { useCallback } from "react";
 import { connect } from "react-redux";
-import { completeProject } from "../redux/actions";
+import { addCompleted } from "../redux/actions";
 import { addArt } from "../redux/actions";
 import ChallengeDisplay from "./ChallengeDisplay";
 import useAPI from "../hooks/useAPI";
+import NewChallenge from "./NewChallenge";
+import Box from "@mui/material/Box";
 
-const ChallengePage = ({ projects, completeProject, addArt }) => {
-    const { markComplete } = useAPI();
-    const markCompleted = useCallback(async (id) => {
-        const res = await markComplete(id);
-        if (!res.data.success) {
-            console.log(res.data.error);
-        } else {
-            completeProject(res.data.data);
-            addArt(res.data.data);
-        }
-    }, []);
-    return (
-        <div>
-            <h2>Challenges:</h2>
-            {projects.map((val) => (
-                <ChallengeDisplay
-                    key={val.id}
-                    project={val}
-                    markCompleted={markCompleted}
-                />
-            ))}
-        </div>
-    );
+const ChallengePage = ({
+  projects,
+  addCompleted,
+  addArt,
+  completedProjects,
+}) => {
+  const { markComplete } = useAPI();
+  const markCompleted = useCallback(async (project) => {
+    const res = await markComplete(project);
+    if (!res.data.success) {
+      console.log(res.data.error);
+    } else {
+      addCompleted(project);
+      addArt(res.data.data);
+      console.log(completedProjects);
+    }
+  }, []);
+  return (
+    <div className="content">
+      <NewChallenge />
+      <h2>Challenges:</h2>
+      <Box sx={{ height: 475, overflowY: "scroll" }}>
+        {projects.map((val) => (
+          <ChallengeDisplay
+            key={val.id}
+            project={val}
+            markCompleted={markCompleted}
+          />
+        ))}
+      </Box>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        projects: state.challenge.projects,
-    };
+  return {
+    projects: state.challenge.projects,
+    completedProjects: state.challenge.completedProjects,
+  };
 };
 const mapDispatchToProps = {
-    completeProject,
-    addArt,
+  addCompleted,
+  addArt,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChallengePage);
