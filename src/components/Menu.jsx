226b7c10bdd9a65de8851f153e6bbbe36f1d3207
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-07 20:49:02 -0500

    adds app bar when logged in

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -25,83 +25,81 @@ const Menu = ({ user }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
   return (
     <>
-      {!user && (
-        <>
-          <AppBar position="static">
-            <Box display="flex" juytifyContent="center" alignItems="center">
-              <Toolbar>
-                <h2 className="textColor">Van Gogh Get Your Work Done</h2>
-              </Toolbar>
-            </Box>
-          </AppBar>
-        </>
-      )}
-      {user && (
-        <>
-          <IconButton size="large" onClick={() => setDrawerOpen(true)}>
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            anchor="left"
-            open={drawerOpen}
-            onClose={() => setDrawerOpen(false)}
-          >
-            <Box width=" 250px">
-              <List>
-                <NavLink
-                  to="challenge"
-                  className="noTextDecor"
-                  onClick={() => setDrawerOpen(false)}
-                >
-                  <ListItem button>
-                    <ListItemIcon>
-                      <AddCircleOutlineIcon />
-                    </ListItemIcon>
-                    <ListItemText>CHALLENGE</ListItemText>
-                  </ListItem>
-                </NavLink>
-                <NavLink
-                  to="museum"
-                  className="noTextDecor"
-                  onClick={() => setDrawerOpen(false)}
-                >
-                  <ListItem button>
-                    <ListItemIcon>
-                      <AccountBalanceIcon />
-                    </ListItemIcon>
-                    <ListItemText>MUSEUM</ListItemText>
-                  </ListItem>
-                </NavLink>
-                <NavLink
-                  to="history"
-                  className="noTextDecor"
-                  onClick={() => setDrawerOpen(false)}
-                >
-                  <ListItem button>
-                    <ListItemIcon>
-                      <PlaylistAddCheckIcon />
-                    </ListItemIcon>
-                    <ListItemText>HISTORY</ListItemText>
-                  </ListItem>
-                </NavLink>
-                <Divider />
-                <NavLink
-                  to="login"
-                  className="noTextDecor"
-                  onClick={() => setDrawerOpen(false)}
-                >
-                  <ListItem button>
-                    <ListItemIcon>
-                      <LogoutIcon />
-                    </ListItemIcon>
-                    <ListItemText>LOGOUT</ListItemText>
-                  </ListItem>
-                </NavLink>
-              </List>
-            </Box>
-          </Drawer>
-        </>
-      )}
+      <>
+        <AppBar position="static">
+          <Box display="flex">
+            <Toolbar>
+              {user && (
+                <>
+                  <IconButton onClick={() => setDrawerOpen(true)}>
+                    <MenuIcon />
+                  </IconButton>
+                  <Drawer
+                    anchor="left"
+                    open={drawerOpen}
+                    onClose={() => setDrawerOpen(false)}
+                  >
+                    <Box width=" 250px">
+                      <List>
+                        <NavLink
+                          to="challenge"
+                          className="noTextDecor"
+                          onClick={() => setDrawerOpen(false)}
+                        >
+                          <ListItem button>
+                            <ListItemIcon>
+                              <AddCircleOutlineIcon />
+                            </ListItemIcon>
+                            <ListItemText>CHALLENGE</ListItemText>
+                          </ListItem>
+                        </NavLink>
+                        <NavLink
+                          to="museum"
+                          className="noTextDecor"
+                          onClick={() => setDrawerOpen(false)}
+                        >
+                          <ListItem button>
+                            <ListItemIcon>
+                              <AccountBalanceIcon />
+                            </ListItemIcon>
+                            <ListItemText>MUSEUM</ListItemText>
+                          </ListItem>
+                        </NavLink>
+                        <NavLink
+                          to="history"
+                          className="noTextDecor"
+                          onClick={() => setDrawerOpen(false)}
+                        >
+                          <ListItem button>
+                            <ListItemIcon>
+                              <PlaylistAddCheckIcon />
+                            </ListItemIcon>
+                            <ListItemText>HISTORY</ListItemText>
+                          </ListItem>
+                        </NavLink>
+                        <Divider />
+                        <NavLink
+                          to="login"
+                          className="noTextDecor"
+                          onClick={() => setDrawerOpen(false)}
+                        >
+                          <ListItem button>
+                            <ListItemIcon>
+                              <LogoutIcon />
+                            </ListItemIcon>
+                            <ListItemText>LOGOUT</ListItemText>
+                          </ListItem>
+                        </NavLink>
+                      </List>
+                    </Box>
+                  </Drawer>
+                </>
+              )}
+              <h2 className="textColor">Van Gogh Get Your Work Done</h2>
+            </Toolbar>
+          </Box>
+        </AppBar>
+      </>
     </>
   );
 };
