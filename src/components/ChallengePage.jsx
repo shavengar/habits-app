7d02a85beb1c075bd2c2c00d57e3783608762df2
Sydney Havengar <sydney.havengar01@gmail.com>
2022-04-07 23:24:07 -0500

    adds remove habit functionality

--- src/components/ChallengePage.jsx
+++ src/components/ChallengePage.jsx
@@ -1,26 +1,38 @@
 import React, { useCallback } from "react";
 import { connect } from "react-redux";
-import { addCompleted } from "../redux/actions";
-import { addArt } from "../redux/actions";
+import { addCompleted, addArt, removeProject } from "../redux/actions";
 import ChallengeDisplay from "./ChallengeDisplay";
 import useAPI from "../hooks/useAPI";
 import NewChallenge from "./NewChallenge";
-import { Paper, Grid, Box } from "@mui/material";
+import { Grid, Box } from "@mui/material";
 
-const ChallengePage = ({ projects, addCompleted, addArt }) => {
-  const { markComplete } = useAPI();
+const ChallengePage = ({ projects, addCompleted, addArt, removeProject }) => {
+  const { markComplete, removeHabit } = useAPI();
   let completedProjects = projects.filter((val) => val.completed === 1);
   let incompleteProjects = projects.filter((val) => val.completed === 0);
 
-  const markCompleted = useCallback(async (project) => {
-    const res = await markComplete(project);
-    if (!res.data.success) {
-      console.log(res.data.error);
-    } else {
-      addCompleted(project);
-      addArt(res.data.data);
-    }
-  }, []);
+  const markCompleted = useCallback(
+    async (project) => {
+      const res = await markComplete(project);
+      if (!res.data.success) {
+        console.log(res.data.error);
+      } else {
+        addCompleted(project);
+        addArt(res.data.data);
+      }
+    },
+    [markComplete, addCompleted, addArt]
+  );
+
+  const removeProj = useCallback(
+    async (id) => {
+      const res = await removeHabit(id);
+      if (res.data.success) {
+        removeProject(id);
+      }
+    },
+    [removeHabit, removeProject]
+  );
   return (
     <Grid container rowSpacing={2} columnSpacing={2} className="projectLayout">
       <Grid item xs={12}>
@@ -43,7 +55,11 @@ const ChallengePage = ({ projects, addCompleted, addArt }) => {
           <h2>Completed Projects:</h2>
           <Box sx={{ height: 475, overflowY: "scroll" }}>
             {completedProjects.map((val) => (
-              <ChallengeDisplay key={val.id} project={val} />
+              <ChallengeDisplay
+                key={val.id}
+                project={val}
+                removeProject={removeProj}
+              />
             ))}
           </Box>
         </div>
@@ -60,6 +76,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = {
   addCompleted,
   addArt,
+  removeProject,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChallengePage);
